f900485b38102c30656aa45e9b6dd401bc87ddc7
kelly4strength <kelly@kellyhoffer.com>
2017-05-30 18:03:34 -0700

    added index and procfile, going through youtube tutorial

---
+++ index.js
@@ -0,0 +1,34 @@
+// index.js
+
+*/ 
+
+// add all of our code
+
+// npm packages
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// this is
+app.use(bodyParser.urlencoded({exteneded: fals}))
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+	res.send('Jello World!')
+})
+
+app.get('/webhook/', function(req, res){
+	if req.query['hub.verify_token'] ===
+		'my_voice_is_my_password_verify_me') {
+			res.send(req.query['hub.challenge'])
+		}
+	res.send('No entry')
+})
+
+app.listen(app.get('port'), function(){
+	console.log('running on port', app.get('port'))
+})
