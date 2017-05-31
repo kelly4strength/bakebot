870dae83d57d756be04c0eb83c099160460bc1f1
kelly4strength <kelly@kellyhoffer.com>
2017-05-31 11:52:53 -0700

    cleaning things up

--- index.js
+++ index.js
@@ -4,24 +4,28 @@
 // 'use strict'
 // add all of our code
 
-// npm packages
-
+// npm packages that we need
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+
+// we will access express through "app"
 const app = express()
 
+// set up our ports, running on heroku but you can run it locally
 app.set('port', (process.env.PORT || 5000))
 
-// this is
+// this is checking for what info is coming in tothe app, seeing if it's the right type
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+// root level function
 app.get('/', function (req, res) {
 	res.send('Jello World!')
 })
 
-app.get('/webhook/', function(req, res){
+// fb is looking for a webhook
+app.get('/webhook/', function (req, res){
 	if req.query['hub.verify_token'] === 
 		// PUT TOKEN IN
 		'my_voice_is_my_password_verify_me') {
