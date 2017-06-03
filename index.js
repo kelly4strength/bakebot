6ce73c1bec595707c254752a7e2213d951b08149
kelly4strength <kelly@kellyhoffer.com>
2017-06-03 10:49:34 -0700

    adding from quickstart for messages

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // add all of our code
 'use strict'
 
-const token = process.env.FB_PAGE_ACCESS_TOKEN
-const vtoken = process.env.FB_VERIFY_ACCESS_TOKEN
+
 
 // npm packages that we need
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+
+// environment variables
+const token = process.env.FB_VERIFY_TOKEN
+const access = process.env.FB_ACCESS_TOKEN
 
 // we will access express through "app"
 const app = express()
@@ -28,15 +31,59 @@ app.get('/', function (req, res) {
 app.get('/webhook/', function (req, res) {
 	// console.log('wtf man why is this still showing got here')
 	// console.log(req.query['hub.verify_token'])
-	if (req.query['hub.verify_token'] === 'ilovepie') {
+	if (req.query['hub.verify_token'] === token) {
 		res.send(req.query['hub.challenge'])
 		return
 	} 
 	res.send('nope"')
 })
 
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+  
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  console.log("Message data: ", event.message);
+}
+
 // adding the server
 // go and get port, and the function will print running on port
 app.listen(app.get('port'), function(){
 	console.log('running on port', app.get('port'))
 })
+
+
+
+
+
+
+
+
+
